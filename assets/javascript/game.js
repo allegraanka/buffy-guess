6b2fb501e80564322394650ae666f09d8f9e7ba2
Allegra Anka <allegra.anka@gmail.com>
2019-02-17 15:47:49 -0500

    added basic game board, no styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,9 @@
 console.log("Loaded");
+
+// start a new game, display game board on-click
+var gameBoard = document.getElementById("gameBoard");
+var startButton = document.getElementById("startButton");
+
+startButton.addEventListener("click", function() {
+    gameBoard.style.display = "block";
+});
